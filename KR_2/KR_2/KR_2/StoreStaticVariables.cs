0687dea58353dffe0b2ed4554c422e8d05664839
RAMirzaliev <whdeserteagle@gmail.com>
2016-03-10 17:50:58 +0500

    Added new functionality to Clients table. Now you can add and dele entries from the table. v2

---
+++ KR_2/KR_2/KR_2/StoreStaticVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KR_2
+{
+    public static class StoreStaticVariables
+    {
+        public static string pathToClientDB = "D:\\OmelchenkoAS_CourseWork\\KR_2\\KR_2\\database\\Client_Omelchenko.csv";
+    }
+}
